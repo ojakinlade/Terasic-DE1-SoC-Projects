fc5701afec6e1b273365ed0a33de578da4d4c2fd
Joseph Akinlade <ojakinlade@gmail.com>
2024-09-26 23:02:51 +0100

    update

--- Projects/P6_VGA/simulation/questa/vga_top_tb.vhd
+++ Projects/P6_VGA/simulation/questa/vga_top_tb.vhd
@@ -9,24 +9,21 @@ end vga_top_tb;
 architecture vga_top_behav of vga_top_tb is
    constant CLK_PERIOD: time := 20 ns;
    signal clk: std_logic := '0';
+	signal pixel_tick: std_logic;
+	signal video_on: std_logic;
    signal KEY: std_logic_vector(3 downto 0);
    signal hsync: std_logic;
    signal vsync: std_logic;
 	signal vga_r: std_logic_vector(7 downto 0);
 	signal vga_g: std_logic_vector(7 downto 0);
 	signal vga_b: std_logic_vector(7 downto 0);
-   signal hcount: std_logic_vector(10 downto 0);
-   signal vcount: std_logic_vector(10 downto 0);
-   signal frame_start: std_logic;
-   signal frame_end: std_logic;
    signal end_test: std_logic := '0';
 begin
    uut: entity work.vga_top(vga_top_rtl)
       generic map(COLOR_DEPTH => 12)
       port map(CLOCK_50 => clk, KEY => KEY, VGA_HS => hsync,
-      VGA_VS => vsync, VGA_R => vga_r, VGA_G => vga_g, 
-		VGA_B => vga_b, hcount => hcount, vcount => vcount, 
-		frame_start => frame_start, frame_end => frame_end);
+      VGA_VS => vsync, VGA_R => vga_r, VGA_G => vga_g, video_on => video_on,
+		VGA_B => vga_b, VGA_CLK => pixel_tick);
         
    reset: KEY(0) <= '0', '1' after CLK_PERIOD;
    
@@ -38,8 +35,8 @@ begin
 
    stimuli: process
    begin
-      wait until frame_start = '1';
-      wait until frame_end = '1';
+      wait until video_on = '1';
+		wait until video_on = '0';
       wait for CLK_PERIOD;
       end_test <= '1';
    end process;
@@ -47,7 +44,7 @@ begin
    output_report: process
    begin 
       wait until KEY(0) = '1';
-      wait until frame_end = '1';
+     -- wait until frame_end = '1';
       report "VGA frame completed successfully.";
         
       wait until end_test = '1';
--- Projects/P6_VGA/src/vga_top.vhd
+++ Projects/P6_VGA/src/vga_top.vhd
@@ -3,86 +3,52 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity vga_top is
-   generic(COLOR_DEPTH: integer := 12);
+   generic(COLOR_DEPTH: integer := 24);
    port(CLOCK_50   : in std_logic;
         KEY        : in std_logic_vector(3 downto 0);
         VGA_HS     : out std_logic;
         VGA_VS     : out std_logic;
-        frame_start: out std_logic;
-        frame_end  : out std_logic;
-        VGA_R    	 : out std_logic_vector(7 downto 0);
-		  VGA_G      : out std_logic_vector(7 downto 0);
-		  VGA_B      : out std_logic_vector(7 downto 0);
-        hcount     : out std_logic_vector(10 downto 0);
-        vcount     : out std_logic_vector(10 downto 0));
+        VGA_CLK    : out std_logic;
+        video_on   : out std_logic;
+        VGA_R      : out std_logic_vector(7 downto 0);
+        VGA_G      : out std_logic_vector(7 downto 0);
+        VGA_B      : out std_logic_vector(7 downto 0));
 end vga_top;
 
 architecture vga_top_rtl of vga_top is
-   -- vga 640x480 parameters
-   constant HD: integer := 640;         -- Horizontal diaplay area
-   constant HF: integer := 16;          -- Horizontal front porch
-   constant HB: integer := 48;          -- Horizontal back porch
-   constant HR: integer := 96;          -- Horizontal retrace
-   constant HT: integer := HD+HF+HB+HR; -- Horizontal total
-   constant VD: integer := 480;         -- Vertical diaplay area
-   constant VF: integer := 10;          -- Vertical front porch
-   constant VB: integer := 33;          -- Vertical back porch
-   constant VR: integer := 2;           -- Vertical retrace
-   constant VT: integer := VD+VF+VB+VR; -- Vertical total
-   -- sync counter and signals
-   signal hcnt, vcnt : std_logic_vector(10 downto 0);
-   signal hsync_i    : std_logic;
-   signal vsync_i    : std_logic;
-   signal video_on_i : std_logic;
-   signal q_reg      : unsigned(1 downto 0);
-   signal tick_25M   : std_logic;
-   signal vga_in     : std_logic_vector(COLOR_DEPTH - 1 downto 0);
+   signal pixel_x: std_logic_vector(9 downto 0); 
+   signal pixel_y: std_logic_vector(9 downto 0);
+   signal q_reg   : std_logic;
+   signal pixel_tick: std_logic;
+   signal rgb_out: std_logic_vector(23 downto 0);
+   signal rgb_reg: std_logic_vector(23 downto 0); 
+   signal rgb_next: std_logic_vector(23 downto 0);
 begin
-   counter_unit: entity work.frame_counter(frame_counter_rtl)
-      generic map(HMAX => HT, VMAX => VT)
-      port map(clk => CLOCK_50, rst_n => KEY(0), sync_clr => '0',
-      h_count => hcnt, v_count => vcnt, inc => tick_25M,
-      frame_start => frame_start, frame_end => frame_end);
-        
+    -- Instantiate VGA sync
+   vga_sync_unit: entity work.vga_sync(vga_sync_rtl)
+   port map(rst_n => KEY(0), clk => CLOCK_50, video_on => video_on,
+            p_tick => pixel_tick, hsync => VGA_HS, vsync => VGA_VS,
+            pixel_x => pixel_x, pixel_y => pixel_y);
+            
    pixel_generate: entity work.pixel_gen(pixel_gen_rtl)
-      port map(h_count => hcnt, v_count => vcnt, rgb => vga_in);
-        
-   TICK_25MHZ: process(CLOCK_50, KEY(0))
+      port map(pixel_x => pixel_x, pixel_y => pixel_y, rgb_out => rgb_out);
+      
+   VGA_CLK <= pixel_tick;
+    
+   rgb_output: rgb_next <= rgb_out when pixel_tick = '1' 
+               else rgb_reg; 
+   
+   -- Buffered ouputs
+   VGA_R <= rgb_reg(23 downto 16); 
+   VGA_G <= rgb_reg(15 downto 8); 
+   VGA_B <= rgb_reg(7 downto 0); 
+   
+   rgb_register: process(CLOCK_50, KEY(0))
    begin
       if KEY(0) = '0' then
-         q_reg <= (others => '0');
+         rgb_reg <= (others => '0');
       elsif rising_edge(CLOCK_50) then
-         q_reg <= (q_reg + 1) mod 2;
-      end if;
-   end process;
-    
-   tick_25M <= '1' when q_reg = "01" else '0';
-
-   hcount <= hcnt;
-   vcount <= vcnt;
-    
-   -- Horizontal sync decoding
-   hsync_i <= '0' when (unsigned(hcnt) >= HD+HF) and (unsigned(hcnt) <= HD+HF+HR-1) else '1';
-   -- Vertical sync decoding
-   vsync_i <= '0' when (unsigned(vcnt) >= VD+VF) and (unsigned(vcnt) <= VD+VF+VR-1) else '1';
-   -- Display on/off
-   video_on_i <= '1' when (unsigned(hcnt) < HD) and (unsigned(vcnt) < VD) else '0';
-    
-   -- Buffered output to VGA monitor
-   process(CLOCK_50)
-   begin
-      if rising_edge(CLOCK_50) then
-         VGA_VS <= vsync_i;
-         VGA_HS <= hsync_i;
-         if video_on_i = '1' then
-            VGA_R <= "0000" & vga_in(11 downto 8);
-				VGA_G <= "0000" & vga_in(7 downto 4);
-				VGA_B <= "0000" & vga_in(3 downto 0);
-         else	-- Black when display off
-            VGA_R <= (others => '0');
-				VGA_G <= (others => '0');
-				VGA_B <= (others => '0');  
-         end if;   
+         rgb_reg <= rgb_next;
       end if;
    end process;
 end vga_top_rtl;
